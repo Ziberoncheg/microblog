cc1fd9ae860efed63a103312e2f69206d4b070e3
ziberon <zzevr.pro@gmail.com>
2017-09-21 16:54:35 +0300

    asdasd

--- tzproj/settings.py
+++ tzproj/settings.py
@@ -96,11 +96,13 @@ WSGI_APPLICATION = 'tzproj.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 #
 DATABASES = {
+    
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
 }
+
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
