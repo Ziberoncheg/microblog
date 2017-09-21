d6c704860f419072bd8efec37953c5b7c321ae27
ziberon <zzevr.pro@gmail.com>
2017-09-21 16:59:41 +0300

    add logs

--- tzproj/settings.py
+++ tzproj/settings.py
@@ -95,16 +95,20 @@ WSGI_APPLICATION = 'tzproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 #
-DATABASES = {
-    
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-}
+# DATABASES = {
+#
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     # }
+# }
+#
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+ 'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
